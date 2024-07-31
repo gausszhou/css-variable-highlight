3c98caa9c9739206fcf973004802dcce77b6daf4
gausszhou <gausszhou@qq.com>
2024-07-31 12:55:47 +0800

    feat: add more test case

---
+++ src/test/variables-use.vue
@@ -0,0 +1,31 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.test-color {
+  color: var(--test-color);
+}
+
+.gray {
+  color: $gray;
+}
+
+.gray1 {
+  color: $gray_1;
+}
+
+.gray2 {
+  color: $gray_2;
+}
+
+.gray3 {
+  color: $gray_3;
+}
+</style>
